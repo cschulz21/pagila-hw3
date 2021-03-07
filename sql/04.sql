90f89f97fd5421bd722901353503d40e032142b7
cschulz21 <cschulz21@cmc.edu>
2021-03-06 19:23:33 -0800

    first commit

--- sql/04.sql
+++ sql/04.sql
@@ -3,12 +3,12 @@
  */
 SELECT DISTINCT
     first_name || ' ' || last_name AS "Actor Name"
-FROM
-(SELECT
-    film_id,
-    unnest(special_features)
-FROM film
-) as t
+FROM(
+    SELECT
+        film_id,
+        unnest(special_features)
+    FROM film
+    ) AS t
 INNER JOIN film_actor USING (film_id)
 INNER JOIN actor USING (actor_id)
-where unnest= 'Behind the Scenes';
+WHERE unnest = 'Behind the Scenes';
